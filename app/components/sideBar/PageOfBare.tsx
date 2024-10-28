c1286c77bb4ef6dbf3af632c90ff28824fa19670
hilalimohamed <125507958+hilalimohamed@users.noreply.github.com>
2024-10-28 20:21:47 +0100

    deploy

--- app/components/sideBar/PageOfBare.tsx
+++ app/components/sideBar/PageOfBare.tsx
@@ -15,7 +15,7 @@ interface useresd {
   user: User
 }
 
-export default function page({ user }: { user: any }) {
+export default function Page({ user }: { user: any }) {
   const elements = useLink()
 
   return (
